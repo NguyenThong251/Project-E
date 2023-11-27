1878b216a2cbdef01c0236e12f11222f58582cea
DatProVjp <tuyendat2421@gmail.com>
2023-11-27 11:03:28 +0700

    slider

--- layout/assets/js/index.js
+++ layout/assets/js/index.js
@@ -1,5 +1,43 @@
+
+
+// SMOOTH SCROLL
+const lenis = new Lenis()
+
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+
 const passwordInput = document.getElementById("passwordInput");
 const togglePassword = document.getElementById("togglePassword");
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 2,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  draggable:true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 togglePassword.addEventListener("click", () => {
   if (passwordInput.type === "password") {
@@ -10,3 +48,4 @@ togglePassword.addEventListener("click", () => {
     togglePassword.setAttribute("name", "eye-outline");
   }
 });
+
