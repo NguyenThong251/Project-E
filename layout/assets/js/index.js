42e6b076c130a201e83deb027abd6a4de80b13e8
myNemisJeff <datphan@gmail.com>
2023-11-29 00:05:04 +0700

    reponsive home

--- layout/assets/js/index.js
+++ layout/assets/js/index.js
@@ -11,33 +11,12 @@ function raf(time) {
 requestAnimationFrame(raf)
 
 
+
 const passwordInput = document.getElementById("passwordInput");
 const togglePassword = document.getElementById("togglePassword");
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  slidesPerView: 2,
-  loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
 
-  // Navigation arrows
-  draggable:true,
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
 
 togglePassword.addEventListener("click", () => {
   if (passwordInput.type === "password") {
