b1e1350ef07618eefaf4498d09910adc322d24d0
unknown <me@here.com>
2023-11-29 09:05:31 +0700

    update29-11

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    ob_start();
+include "View/header.php";
+if (!isset($_GET['pg'])) {
+    include "View/home.php";                                                
+}
+else {
+    switch ($_GET['pg']) {
+        case 'sanpham':
+           include "View/sanpham.php";
+            break;
+        default:
+        include "View/home.php";
+            break;
+    }
+}
+
+include "View/footer.php";
+
+?>
